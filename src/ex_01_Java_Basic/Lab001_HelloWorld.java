4f4ed7d23ec5a019113d5edabad38d1649410756
Mansoor <mansoor.mm0522@gmail.com>
2025-09-08 22:13:13 +0530

    Directory commit

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -4,5 +4,6 @@ public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         //commit new chnages
+        System.out.println("1+1=2");
     }
 }
