2605fea5e363f5dca875e28c32b4a29220ebfccd
Mansoor-KS <mansoor.mm0522@gmail.com>
2025-09-14 23:43:56 +0530

    Update Nine_table.java
    Comments

--- src/ex_01_Java_Basic/Nine_table.java
+++ src/ex_01_Java_Basic/Nine_table.java
@@ -4,16 +4,16 @@ public class Nine_table {
     public static void main(String[] args) {
         int tab = 9;
         System.out.printf("%d X 1 = %d", tab, tab*1);
-        System.out.printf("%n");
+        System.out.printf("%n"); // Used two ways for new line.
         System.out.printf("%d x 2 = %d", tab, tab*2);
-        System.out.printf("%n");
+        System.out.printf("%n"); //%n in seperate line
         System.out.printf("%d x 3 = %d", tab,tab*3);
         System.out.printf("%n");
         System.out.printf("%d x 4 = %d", tab, tab*4);
         System.out.printf("%n");
         System.out.printf("%d x 5 = %d", tab, tab*5);
         System.out.printf("%n");
-        System.out.printf("%d x 6 = %d%n", tab, tab*6);
+        System.out.printf("%d x 6 = %d%n", tab, tab*6); //%n in same line.
         System.out.printf("%d x 7 = %d%n", tab, tab*7);
         System.out.printf("%d x 8 = %d%n", tab, tab*8);
         System.out.printf("%d x 9 = %d%n", tab, tab*9);
