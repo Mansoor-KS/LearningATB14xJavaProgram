7decf23e6954abc0ce5fbc6e6dfe11bd273b479c
Mansoor <mansoor.mm0522@gmail.com>
2025-10-12 11:53:29 +0530

    Challeng 1

---
+++ src/Task_Test/Challenge_1_Declaration_initilaisation.java
@@ -0,0 +1,14 @@
+package Task_Test;
+
+public class Challenge_1_Declaration_initilaisation {
+    public static void main(String[] args){
+        int a = 25;
+        double d = 3.14;
+        boolean b = true;
+        char c = 'A';
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + d);
+        System.out.println("Boolean: "+b);
+        System.out.println("Character: " +c);
+    }
+}
