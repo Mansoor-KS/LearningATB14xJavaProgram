3d2b1b9fc1aa7e7beda79b93a4d4deff5b8d3d2c
Mansoor <mansoor.mm0522@gmail.com>
2025-10-12 12:21:00 +0530

    Challeng 2

---
+++ src/Task_Test/Challeng_2_Arthmatic_Op.java
@@ -0,0 +1,21 @@
+package Task_Test;
+
+public class Challeng_2_Arthmatic_Op {
+    public static void main(String[]args){
+        int a =  10;
+        int b = 5;
+        System.out.println("Addition: " + a+b);
+        System.out.println("Subtraction: " + (a-b));
+        System.out.println("Multipliction: " + (a*b));
+        System.out.println("Division: " + (a/b));
+        System.out.println("Modolus: "+ (a%b));
+
+        System.out.println("-----------Demonstration of Operators---------");
+        System.out.println(a=b);
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+        System.out.println(a%=b);
+    }
+}
