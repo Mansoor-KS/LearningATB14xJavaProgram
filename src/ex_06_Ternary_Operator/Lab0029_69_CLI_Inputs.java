aab7398772144fc1f3236aedad3232cd801201e5
Mansoor-KS <mansoor.mm0522@gmail.com>
2025-10-15 21:21:35 +0530

    11th & 12th

--- src/ex_06_Ternary_Operator/Lab0029_69_CLI_Inputs.java
+++ src/ex_06_Ternary_Operator/Lab0029_69_CLI_Inputs.java
@@ -9,7 +9,7 @@ public class Lab0029_69_CLI_Inputs {
         String up3 = args[2];
         String up4 = args[3];
         String up5 = args[4];
-       // String up6 = args[5]; // There is no value in args inCL for this this throw error
+        //String up6 = args[5]; // There is no value in args inCL for this this throw error
 
         System.out.println(up1 + up2 + up3);
         System.out.println(up1);
@@ -17,6 +17,6 @@ public class Lab0029_69_CLI_Inputs {
         System.out.println(up3);
         System.out.println(up4);
         System.out.println(up5);
-        //System.out.println(up6);
+       //System.out.println(up6);
     }
 }
